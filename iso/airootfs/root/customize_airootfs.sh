c732a5c97f9fb14c74c2823f425753003d165100
Andrianos papamarkou <papamarkoua@gmail.com>
2025-11-30 20:27:44 +0200

    build fixes

--- iso/airootfs/root/customize_airootfs.sh
+++ iso/airootfs/root/customize_airootfs.sh
@@ -21,10 +21,10 @@ chmod a+x /home/user/.local/bin/* 2>/dev/null || true
 chmod a+x /home/user/.local/bin/appimage-integrator/* 2>/dev/null || true
 chmod a+x /usr/local/bin/* 2>/dev/null || true
 
-# sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-# sed -i 's/^#el_GR.UTF-8 UTF-8/el_GR.UTF-8 UTF-8/' /etc/locale.gen
-# locale-gen
-# echo "LANG=en_US.UTF-8" > /etc/locale.conf
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i 's/^#el_GR.UTF-8 UTF-8/el_GR.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 
 # Enable services
